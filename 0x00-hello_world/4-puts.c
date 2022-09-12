c1182c0d367bf98be670cdb4a6268a5c9ccda692
Drpossible <victoruchenna.nwaoha@gmail.com>
2022-09-12 07:09:48 -0700

    helloput

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdio.h>
 /**
- * description: main - prints out a string to stdout.
+ * main - Entry point
  *
- * return: 0 always (success)
- *
+ * Return: Always 0
  */
+
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle")
-return (0);
+	("\"programming is like building a multilingual puzzle")
+		return (0);
 }
