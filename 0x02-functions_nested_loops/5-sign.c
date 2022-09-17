37732d79946912e6b92ce198a2f1fd9441396244
Drpossible <victoruchenna.nwaoha@gmail.com>
2022-09-17 08:10:03 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - sign of a number
+ *
+ * @n: n is the integer
+ * Return: 1 and print +, 0 print 0, -1 print -
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putcharb('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
+}
