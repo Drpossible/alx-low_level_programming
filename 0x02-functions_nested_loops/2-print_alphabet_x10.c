f59c3018acf97b27f81d43f2a397de9659e82861
Drpossible <victoruchenna.nwaoha@gmail.com>
2022-09-17 06:25:10 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - 10 times Alphabet in lower case
+ *
+ * Return:0
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int a = 0;
+
+	while (a < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{ 
+			_putchar(c);
+		}
+
+		_putchar('\n');
+		a++;
+	}
+}
